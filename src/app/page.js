a010acd1336f5e14fd2acb3528958dbb5f02fa5c
Gowthamap <gowthamap55555@gmail.com>
2023-10-07 17:17:53 +0530

    feat: basic setup of next project

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Footer from './components/footer'
+import Header from './components/header'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header />
+      <Footer />
+    </main>
+  )
+}
